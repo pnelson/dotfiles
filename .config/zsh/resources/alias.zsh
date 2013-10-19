cabc1d6920f069c7deab5ce88a6f2081bd86fa30
Philip Nelson <me@pnelson.ca>
2013-10-18 18:10:07 -0700

    Safe cp, mv, and rm.

--- .config/zsh/resources/alias.zsh
+++ .config/zsh/resources/alias.zsh
@@ -1,5 +1,8 @@
 alias dots="git --git-dir=$HOME/Development/dotfiles --work-tree=$HOME"
 
+alias cp='cp --interactive'
+alias mv='mv --interactive'
+alias rm='rm --interactive'
+
 alias cl='clear'
 alias ls='LC_COLLATE="C" ls --color=auto --group-directories-first --human-readable'
-alias rm='rm -i'
