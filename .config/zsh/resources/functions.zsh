89392eb3f492da7a2c9ef02aeb6912ae9e994385
Philip Nelson <me@pnelson.ca>
2014-02-23 11:07:07 -0800

    Add some Go project zsh functions. This is terrible.

--- .config/zsh/resources/functions.zsh
+++ .config/zsh/resources/functions.zsh
@@ -3,6 +3,11 @@ function ff() {
   find . -name $1
 }
 
+# change into project specific go dir
+function cdgodir() {
+  cd $(find "$1/src/" -type d -name "$1")
+}
+
 # estimate file space usage for each dir in current directory
 function lsdir() {
   for dir in *; do
@@ -10,6 +15,13 @@ function lsdir() {
       du --human-readable --summarize --dereference $dir 2> /dev/null
     fi
   done
+}
+
+# make a project specific go dir
+function mkgodir() {
+  mkdir -p "$1/src/github.com/$USER/$1"
+  echo "export GOPATH=\"\$PWD\"\nexport PATH=\"\$GOPATH/bin:\$PATH\"" > "$1/.envrc"
+  cd "$1/src/github.com/$USER/$1"
 }
 
 # reset permissions of all files and directories in the current directory
