3eca7b15c2c5167e4c12b07df9317f2b270e8619
Philip Nelson <me@pnelson.ca>
2014-08-29 18:45:16 -0700

    Add cal and date zsh alias.

--- .config/zsh/resources/alias.zsh
+++ .config/zsh/resources/alias.zsh
@@ -5,7 +5,9 @@ alias ls='LC_COLLATE="C" ls --color=auto --group-directories-first --human-reada
 alias md='mkdcd'
 
 alias lsgroup='cut --delimiter=: --fields=1 /etc/group'
+alias now='date --utc +%Y%m%dT%H%M%SZ'
 
+alias cal='cal -A 2'
 alias tree='tree --dirsfirst'
 alias wcalc='wcalc --quiet'
 alias weechat='weechat --dir "$XDG_CONFIG_HOME/weechat"'
